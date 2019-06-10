c95961b2832973fd32dec982e61944138658a4d2
John Blank <veloblank@gmail.com>
2019-06-10 17:36:06 -0400

    Create Props resource and build seed file to quickly scrape ESPN sports

---
+++ db/migrate/20190610204148_create_props.rb
@@ -0,0 +1,16 @@
+class CreateProps < ActiveRecord::Migration[5.0]
+  def change
+    create_table :props do |t|
+      t.string :title
+      t.string :start_time
+      t.string :sport
+      t.string :home_team
+      t.string :away_team
+      t.boolean :home_team_won?, default: nil
+      t.boolean :away_team_won?, default: nil
+      t.boolean :scored_by_admin?, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+
+Prop.all.each {|p| p.destroy}
+
+ESPN = "http://streak.espn.com/en/"
+@doc = Nokogiri::HTML(open(ESPN))
+props = @doc.css("div .matchup-container")
+
+props.each do |p|
+  prop = Prop.create(
+    title:  p.css(".gamequestion").text,
+    start_time: p.css("div .startTime").text,
+    sport: p.css(".sport-description").text,
+    away_team: p.css("td span strong")[0].text,
+    home_team: p.css("td span strong")[1].text
+  )
+end
