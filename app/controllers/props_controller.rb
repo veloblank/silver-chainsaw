503baf828f149b3466d12a7921db51455b0dfeba
John Blank <veloblank@gmail.com>
2019-06-11 12:27:38 -0400

    Create private method 'prop_params' for CSRF in forms

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -1,2 +1,15 @@
 class PropsController < ApplicationController
+
+  def new
+    @prop = Prop.new
+  end
+
+  private
+  def prop_params
+    params.require(:prop).permit(
+      :title, :start_time, :sport, :home_team, :away_team,
+      :espn_game_identifier, :home_team_won?, :away_team_won?,
+      :scored_by_admin?
+    )
+  end
 end
