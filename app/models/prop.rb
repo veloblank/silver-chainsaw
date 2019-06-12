563f5fe5302bba2b19ac98d55c8f706f8d37eba1
John Blank <veloblank@gmail.com>
2019-06-11 23:48:07 -0400

    Create simple validation from Prop model.

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -2,6 +2,7 @@ class Prop < ApplicationRecord
   belongs_to :board
   has_many :contestant_picks
   has_many :contestants, through: :contestant_picks
+  validates :title, :start_time, :sport, :home_team, :away_team, :board_id, presence: true
 
 
   #this method helps check for duplicates by comparing the object's attributes as a string
