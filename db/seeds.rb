09fa2d3bb2c66fab903190eb4d5d98126a432864
John Blank <veloblank@gmail.com>
2019-06-12 03:44:02 -0400

    Buff scraper to scrape historical data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,39 @@
-#Prop.all.each {|p| p.destroy}
+#-------Can be used to scrape historical data/props to seed db
+#--------------------------------------------------------------
+now = DateTime.now
+ten_days_ago = now - 10
+ary = Array.new(10).map.with_index(1) {|_,index| (ten_days_ago + index).strftime('%Y%m%d')}
+#builds an array of strings formatted as: ["20190603", "20190604", "20190605"......]
 
-#--------------Can be used to scrape historical data/props
-# date = Date.today.strftime('%Y%m%d')
-# change 'date' to_i and subtract number of days and scrape historical
-# ESPN = "http://fantasy.espn.com/streak/en/entry?date=#{date}"
-#-----------------------------------------------------
+ary.each do |i|
+  espn = "http://fantasy.espn.com/streak/en/entry?date=#{i}"
+  @doc = Nokogiri::HTML(open(espn))
+  props = @doc.css(".matchup-container")
 
-ESPN = "http://fantasy.espn.com/streak/en/"
-@doc = Nokogiri::HTML(open(ESPN))
-props = @doc.css(".matchup-container")
+  props.each do |p|
+    date = p.css(".startTime").attr("data-locktime").value
+    board_name = DateTime.parse(date).strftime('%Y%m%d')
+    board = Board.find_or_create_by(name: board_name)
+    prop = Prop.new(
+      title:  p.css(".gamequestion").text,
+      start_time: DateTime.parse(date),
+      sport: p.css(".sport-description").text,
+      away_team: p.css("td span strong")[0].text,
+      home_team: p.css("td span strong")[1].text,
+      board_id: board.id
+    )
+    if p.css(".matchupStatus a").attr("href")
+      prop.espn_game_identifier = p.css(".matchupStatus a").attr("href").value
+    else
+      prop.espn_game_identifier = "Postponed"
+    end
 
-props.each do |p|
-  date = p.css(".startTime").attr("data-locktime").value
-  board_name = DateTime.parse(date).strftime('%Y%m%d')
-  board = Board.find_or_create_by(name: board_name)
-  binding.pry
-  prop = Prop.new(
-    title:  p.css(".gamequestion").text,
-    start_time: DateTime.parse(date),
-    sport: p.css(".sport-description").text,
-    away_team: p.css("td span strong")[0].text,
-    home_team: p.css("td span strong")[1].text,
-    board_id: board.id
-  )
-  if p.css(".matchupStatus a").attr("href")
-    prop.espn_game_identifier = p.css(".matchupStatus a").attr("href").value
-  else
-    prop.espn_game_identifier = "Postponed"
+    unless Prop.all.any? {|db_prop| db_prop.gamecast_s == prop.gamecast_s}
+      prop.save
+    end
   end
 
-  unless Prop.all.any? {|db_prop| db_prop.gamecast_s == prop.gamecast_s}
-    prop.save
-  end
 end
-
 #Faker Contestant Data
 # 10.times do
 #   email = Faker::Internet.free_email
