8bd99b2c4deb8cb26df3274985b38f7eaf809eb0
John Blank <veloblank@gmail.com>
2019-06-12 00:32:19 -0400

    Nest routes. Add view files. Build board show page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,33 @@
 class BoardsController < ApplicationController
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+
+
+  end
+
+  def show
+    @board = Board.find_by(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :scored?)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :boards
-  resources :contestants
+  resources :boards do
+    resources :props, only: %i[show index]
+  end
+
   resources :props
+  resources :contestants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
