914363ba9e8040148eac7e6aa385a504a58af71a
John Blank <veloblank@gmail.com>
2019-06-12 03:01:50 -0400

    Create helper method for boards#show view to provide links to boards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :previous_board, :next_board
+
+  def previous_board
+    @previous_board = Board.where('id < ?', params[:id]).last
+  end
+
+  def next_board
+    @next_board = Board.where('id > ?', params[:id]).first
+  end
 end
