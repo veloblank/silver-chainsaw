11f273e8e42ed9b8cb48064f1b7556da15d28ed5
John Blank <veloblank@gmail.com>
2019-06-17 14:26:36 -0400

    Move date links in board page to helper methods

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,17 @@
 module BoardsHelper
+
+  def previous_board_link
+    if previous_board
+      DateTime.parse(previous_board.name).strftime('%a. %b %e')
+    end
+  end
+
+  def next_board_link
+    if next_board
+      DateTime.parse(next_board.name).strftime('%a. %b %e')
+    end
+  end
+
+
+
 end
