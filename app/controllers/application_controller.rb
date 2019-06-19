be8179227ac95150a6ae01ef15a5304506857cf3
John Blank <veloblank@gmail.com>
2019-06-18 21:45:21 -0400

    Remove helper methods from application contr and include SessionsHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :previous_board, :next_board, :logged_in?, :current_user
+  helper_method :previous_board, :next_board
+  include SessionsHelper
 
   def previous_board
     @previous_board = Board.where('id < ?', params[:id]).last
@@ -10,11 +11,4 @@ class ApplicationController < ActionController::Base
     @next_board = Board.where('id > ?', params[:id]).first
   end
 
-  def logged_in?
-    !!current_user
-  end
-
-  def current_user
-      Contestant.find_by(id: session[:user_id])
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  def log_in(contestant)
+    session[:user_id] = contestant.id
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    #Contestant.find_by(id: session[:user_id]) refactor to not hit DB everytime
+    @current_user ||= Contestant.find_by(id: session[:user_id])
+  end
 end
