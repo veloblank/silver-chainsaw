e94192f15a0a5d4c547b3633f8e36ba74e25aa7b
John Blank <veloblank@gmail.com>
2019-07-02 20:10:34 -0400

    Increment and reset streak methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
   validates :password, length: { minimum: 8, allow_nil: true }, confirmation: true
+
+  def reset_streak
+    self.update(current_streak: 0)
+  end
+
+  def increment_streak
+    streak = current_streak + 1
+    self.update(:current_streak => streak)
+  end
+
+
 end
