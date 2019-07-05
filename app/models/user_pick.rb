205c57211c1bc176d99db0f693c4e501f16cb93f
John Blank <veloblank@gmail.com>
2019-07-05 04:35:35 -0400

    Fix update in score_prop giving erroneous true values for prop won

--- app/models/user_pick.rb
+++ app/models/user_pick.rb
@@ -3,7 +3,6 @@ class UserPick < ApplicationRecord
   belongs_to :prop
 
   def self.score_pick(prop)
-    prop.admin_score
     picks = UserPick.where("prop_id = ?", prop.id)
     picks.each do |pick|
       if prop.away_team_won
@@ -11,22 +10,18 @@ class UserPick < ApplicationRecord
           pick.update(side_won: true, scored: true)
           pick.user.increment_streak
           pick.user.check_best_streak
-          prop.update(:home_team_won => false)
         else
           pick.update(side_won: false, scored: true)
           pick.user.reset_streak
-          prop.update(:home_team_won => true)
         end
       else
         if pick.side == "home"
           pick.update(side_won: true, scored: true)
           pick.user.increment_streak
           pick.user.check_best_streak
-          prop.update(:away_team_won => false)
         else
           pick.update(side_won: false, scored: true)
           pick.user.reset_streak
-          prop.update(:away_team_won => true)
         end
       end
     end
