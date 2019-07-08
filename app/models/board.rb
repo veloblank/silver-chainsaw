cc6c664882d2bbefd8d937a81488c5f8b57ab586
John Blank <veloblank@gmail.com>
2019-07-08 16:49:26 -0400

    Create custom error message that is more UI friendly

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
   has_many :props
   validates :date, presence: true
-  validates :date, uniqueness: true
+  validates_uniqueness_of :date, message: "This prop board has already been created."
 end
