866e4cb55333351e47bb62058197bc6796e64b30
John Blank <veloblank@gmail.com>
2019-07-08 16:49:46 -0400

    Revise method comment

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -9,7 +9,7 @@ class Prop < ApplicationRecord
 
 
   #this method helps check for duplicates by comparing the object's attributes as a string
-  #with other objects in the database
+  #with other objects in the database when creating seed data
   def gamecast_s
     "Title: #{self.title} on ESPN Gamecast: #{self.espn_game_identifier}"
   end
