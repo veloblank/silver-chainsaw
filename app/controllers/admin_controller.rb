dd67876e70fcd719d7c4a0afd4179d3ba1458aba
John Blank <veloblank@gmail.com>
2019-07-09 02:53:08 -0400

    (Refactor) change params of scored props form. Remove redundant method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,8 +6,7 @@ class AdminController < ApplicationController
   end
 
   def change_score
-    prop = Prop.find_by(id: params[:prop][:prop])
-    fill_out_score_params(params)
+    prop = Prop.find(params[:prop][:prop_id])
     if prop.update(score_params)
       prop.admin_score
       UserPick.score_pick(prop)
@@ -16,14 +15,6 @@ class AdminController < ApplicationController
   end
 
   def score_params
-    params.require(:prop).permit(:home_team_won, :away_team_won, :prop_id)
-  end
-
-  def fill_out_score_params(params)
-    if params[:prop][:home_team_won]
-      params[:prop][:away_team_won] = false
-    else
-      params[:prop][:home_team_won] = false
-    end
+    params.require(:prop).permit(:home_team_won, :away_team_won)
   end
 end
