2a623621053b9d9f10409910cf933e7b8a9d7c39
John Blank <veloblank@gmail.com>
2019-07-09 03:56:01 -0400

    Remove flash.now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
         login user
         redirect_to root_path
       else
-        flash.now[:danger] = "The username and password combination you entered is incorrect."
+        flash[:danger] = "The username and password combination you entered is incorrect."
         render :new
       end
     end
