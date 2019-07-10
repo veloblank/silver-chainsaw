8eb59ee3d47858b4a80633579fd8bd3c5cc07555
John Blank <veloblank@gmail.com>
2019-07-10 13:58:21 -0400

    Change name of methods to reflect they are CSS-related to adding class

--- app/helpers/props_helper.rb
+++ app/helpers/props_helper.rb
@@ -8,32 +8,32 @@ module PropsHelper
     selection.save
   end
 
-  def prop_locked(prop)
+  def class_prop_locked(prop)
     prop.lock_prop?
     if prop.locked
       'prop-locked'
     end
   end
 
-  def prop_scored(prop)
+  def class_prop_scored(prop)
     if prop.scored_by_admin
       'prop-scored'
     end
   end
 
-  def prop_selected(prop)
+  def class_prop_selected(prop)
     if logged_in? && user_selection && user_selection.prop_id == prop.id
       'prop-pending'
     end
   end
 
-  def user_won(prop)
+  def class_user_won(prop)
     if logged_in? && current_user.user_picks.any?{|pick| pick.prop_id == prop.id && pick.side_won == true}
       'prop-win'
     end
   end
 
-  def user_lost(prop)
+  def class_user_lost(prop)
     if logged_in? && current_user.user_picks.any?{|pick| pick.prop_id == prop.id && pick.side_won == false}
       'prop-loss'
     end
