503394de607fe05daf2247b4f3d984d837652e8c
John Blank <veloblank@gmail.com>
2019-07-17 13:39:34 -0400

    Remove nested 'new' route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   delete 'logout', to: "sessions#destroy"
 
   resources :boards do
-    resources :props, only: %i[index new]
+    resources :props, only: %i[index]
   end
 
   resources :props
